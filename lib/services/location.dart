d3211fe79b916e61ab51cf467590223e0387bdc0
Virjanand <virjanand@virjanand.com>
2020-03-03 06:31:50 +0100

    refactor get location in separate class

---
+++ lib/services/location.dart
@@ -0,0 +1,21 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  double _longitude;
+  double _latitude;
+
+  Future<void> getCurrentLocation() async {
+    try {
+      Position position = await Geolocator()
+          .getCurrentPosition(desiredAccuracy: LocationAccuracy.lowest);
+      _longitude = position.longitude;
+      _latitude = position.latitude;
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  double get latitude => _latitude;
+
+  double get longitude => _longitude;
+}
