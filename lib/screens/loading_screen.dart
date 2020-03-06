318dca0fdb8b9316c46298e279370ecf6c2cfed2
Virjanand <virjanand@virjanand.com>
2020-03-06 06:08:26 +0100

    cleanup decoding data from openWeather

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,6 @@
 import 'package:clima/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
@@ -22,22 +22,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print("longitude: " + location.longitude.toString());
   }
 
-  Future<void> getData() async {
-    var response = await get(
+  void getData() async {
+    http.Response response = await http.get(
         "https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22");
     if (response.statusCode == 200) {
       String data = response.body;
-      var longitute = jsonDecode(data)['coord']['lon'];
-      var latitude = jsonDecode(data)['coord']['lat'];
-      var weatherDescription = jsonDecode(data)['weather'][0]['description'];
 
-      var temperature = jsonDecode(data)['main']['temp'];
-      var conditionNumber = jsonDecode(data)['weather'][0]['id'];
-      var city = jsonDecode(data)['name'];
+      var decodedData = jsonDecode(data);
+
+      double temperature = decodedData['main']['temp'];
+      int conditionNumber = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
 
       print(temperature);
       print(conditionNumber);
-      print(city);
+      print(cityName);
     } else {
       print(response);
     }
